2cc1e76cb629d725ae85094252c357ce1c891ba6
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-10-20 17:59:23 -0500

    Projeto Concluido

--- desafio23.sql
+++ desafio23.sql
@@ -1,4 +1,4 @@
 UPDATE northwind.order_details
-SET discount = 15.0000
+SET discount = 30
 WHERE unit_price < 10.0000;
 
---
+++ desafio24.sql
@@ -0,0 +1,4 @@
+UPDATE northwind.order_details
+SET discount = 45
+WHERE unit_price > 10.0000 && (id > 29 && id < 41);
+
